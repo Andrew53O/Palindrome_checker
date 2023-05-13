263b6939a8deb90775d743fd9e72c5e5e3c0f175
Andrew Suryadarma Hendry <andrewsuryaaccforprogramming@gmail.com>
2023-05-13 23:24:33 +0800

    adding fil;ter for no input

--- function.js
+++ function.js
@@ -1,13 +1,18 @@
 
-// to do inputted itu udh jadi text blm
-// entar tinggal masukin code yang udh ada dibawah
-
+// user input for the text
 function get_user_input()
 {
     // get the input
     const input_Field = document.getElementById("input-field");
     
     // check the value
+    
+    // check for the null
+    if (!input_Field.value)
+    {
+      alert("Text is empty, Please input some text");
+      return
+    }
     const result = palindrome(String(input_Field.value));
 
     if(result == false)
@@ -21,8 +26,9 @@ function get_user_input()
     setTimeout(() => try_again(), 500);
 }
 
-
+// check for palindrome
 function palindrome(str) {
+  
   let editedStr = str.replace(/\W+|_/g, "").toLowerCase();
   let reversedStr = editedStr.split("").reverse().join("");
   console.log(reversedStr);
@@ -34,21 +40,23 @@ function palindrome(str) {
   return true;
 }
 
+// 
 function try_again()
 {
     const ontinue = confirm("Do you want to try again ?");
 
-    if (ontinue == false)
+    if (ontinue != false)
     {
-        
+      location.reload();
     }
-    else
-    {
-            location.reload();
-    }
+
 }
 
 
+// change input mode
 
+const changeInput = document.getElementsByClassName("change_input");
+
+console.log(changeInput);
 
 
